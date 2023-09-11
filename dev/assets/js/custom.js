1768de759946a662b01cbe6be119e87f2ce23ddd
XelgaAr <143228153+XelgaAr@users.noreply.github.com>
2023-09-11 17:49:39 +0300

    Advantage add

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,3 +2,32 @@
   burger.addEventListener('click', function(){
     burger.classList.toggle('is-active');
   });
+
+
+  mobileOnlySlider("#advantages-slider", true, false, 992);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: $dots,
+    arrows: $arrows,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+} // Mobile Only Slider
